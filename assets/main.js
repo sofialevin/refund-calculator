a063278ebcfa05c1b6f242d8815475cb3bd2fd22
Sofia Levin <slevin@M-4876.local>
2018-08-28 15:13:43 -0700

    Made the calculator work

--- assets/main.js
+++ assets/main.js
@@ -16,3 +16,26 @@ $(function () {
         todayHighlight: true
   }).datepicker('update', new Date());
 });
+
+
+$("#refund").click(function() {
+    var membership = $("#sel1").val();
+  var first = $("#start_date").data("datepicker").getFormattedDate('mm-dd-yyyy');
+  var second = $("#end_date").data("datepicker").getFormattedDate('mm-dd-yyyy');
+
+// new Date("dateString") is browser-dependent and discouraged, so we'll write
+// a simple parse function for U.S. date format (which does no error checking)
+function parseDate(str) {
+    var mdy = str.split('-');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+$("#result").val( '$' + (((datediff(parseDate(first), parseDate(second)))/365) * membership ).toFixed(2));
+
+});
