f7dc0e69f35cac4caf4165735be9bc13f504e7cc
Sofia Levin <slevin@M-4876.local>
2018-10-10 15:53:54 -0700

    Added functional Calculate & Reply button

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,4 @@
-$(function() {
-  var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '200px' });
-});
+var client = ZAFClient.init();
 
 $(function () {
   $("#start_date").datepicker({ 
@@ -17,7 +14,7 @@ $(function () {
   }).datepicker('update', new Date());
 });
 
-$("#refund").click(function() {
+function refundCalculator() {
     var membership = $("#sel1").val();
   var first = $("#start_date").data("datepicker").getFormattedDate('mm-dd-yyyy');
   var second = $("#end_date").data("datepicker").getFormattedDate('mm-dd-yyyy');
@@ -38,4 +35,31 @@ function datediff(first, second) {
 
 $("#result").val('$' + (((datediff(parseDate(first), parseDate(second)))/365) * (membership - discount) ).toFixed(2));
 
-});
+}
+
+$("#refund").click(refundCalculator);
+
+$("#reply").click(function() {
+
+refundCalculator();
+client.invoke('ticket.comment.appendHtml', '<p>Hi {{ticket.requester.first_name}}‍,</p><br> <p>I have completed the pro-rated refund in the amount of <b>' + $("#result").val() + '</b>. It should fully process in 5-10 business days.</p><br> <p>Let us know if you need help with anything else on this request. Have a great week!</p><br> <p>Regards,<br>{{current_user.first_name}}‍</p>');
+
+
+
+
+
+
+
+client.set('ticket.customField:custom_field_360000188703', "category__chore")
+client.set('ticket.customField:custom_field_360000187006', "memberships__refunds__partial_refund")
+
+})
+
+
+
+
+
+
+
+
+
