6b620330a20f2e4f63eb0cf521dfceca41eb61bd
Sofia Levin <slevin@M-4876.local>
2019-02-19 15:47:36 -0800

    Refactored to have reusable getData function and made app retrieve charge IDs instead if invoice IDs.

--- assets/main.js
+++ assets/main.js
@@ -4,46 +4,43 @@ var client = ZAFClient.init()
 
 client.invoke('resize', { width: '100%', height: '300px' })
 
-$(document).ready(function() {
-  $(".btn").click(function() {
-    var $this = $(this);
-    $this.button('loading');
-    $("#sub_results").empty(
-      );
-    const subscription = $("#subscriptionID").val();
-    const Url =
-      "https://api.stripe.com/v1/invoices?subscription=" + subscription;
-    $.ajax({
-      url: Url,
-      type: "GET",
-      headers: {
-        Authorization: "Bearer sk_test_Op7lb2k7gDf4C2fxSq3KjVxm"
-      },
-      success: function(result) {
-        $this.button('reset');
-        $("#sub_results").append(
-          '<div id="invoices"></div>'
-          );
-        var invoicesList = result.data;
-        var invoice;
-        for (invoice in invoicesList) {
-          var id = invoicesList[invoice].id;
-          var date = invoicesList[invoice].date;
+function getData(Url) {
+  return $.ajax({
+    url: Url,
+    type: "GET",
+    headers: {
+      Authorization: "Bearer sk_test_Op7lb2k7gDf4C2fxSq3KjVxm"
+    },
+  });
+}
+
+  $(document).ready(function() {
+    $(".btn").click(function() {
+      var $this = $(this);
+      $this.button("loading");
+      $("#sub_results").empty();
+      const subscriptionID = $("#subscriptionID").val();
+      const subUrl =
+            "https://api.stripe.com/v1/invoices?subscription=" + subscriptionID;
+
+      getData(subUrl).done(function(result) {
+        $this.button("reset");
+        $("#sub_results").append('<div id="invoices"></div>');
+        var subscriptionData = result.data;
+        var id;
+        for (id in subscriptionData) {
+          const chargeID = subscriptionData[id].charge;
+
           $("#invoices").append(
-            '<button class="btn btn-outline-info" data-toggle="collapse" data-target="#' + id + '">' +
-              id +
-              " " +
-              date +
-              '</button><div id="' + id + '" class="collapse">Lorem ipsum dolor text....</div>'
+            '<button class="btn btn-outline-info" data-toggle="collapse" data-target="#' +
+            chargeID +
+            '">' +
+            chargeID +
+            '</button><div id="' +
+            chargeID +
+            '" class="collapse">Lorem ipsum dolor text....</div>'
           );
         }
-      },
-      error: function(error) {
-        console.log(`Error${error}`);
-      }
+      });
     });
   });
-});
-
-
-
